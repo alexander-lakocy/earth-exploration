22764c034879339ac95c3869ba2300e3a4610559
alakocy <alakocy@gmail.com>
2024-11-23 13:52:18 -0800

    [ENH] Add ASFDataFile class and associated tests

--- src/asf_analysis/asf_data.py
+++ src/asf_analysis/asf_data.py
@@ -1,8 +1,107 @@
 from pathlib import Path
+from typing import Union
+
+import xml.etree.ElementTree as ET
+
+import numpy as np
+
+ASF_CHUNK_ORDER = [
+    "mission",
+    "beam_mode",
+    "product_type_resolution",
+    "process_level_class_polarization",
+    "start_date_time",
+    "end_date_time",
+    "absolute_orbit_number",
+    "mission_data_take_id",
+    "product_unique_identifier",
+]
+
+
+def parse_asf_data_from_filename(filename: Union[str, Path]) -> dict:
+    if not isinstance(filename, Path):
+        try:
+            filepath = Path(filename)
+        except TypeError as te:
+            raise TypeError(f"Unable to convert input (type: {type(filename)}) to path. Supply input as str or Path.")
+
+    else:
+        filepath = filename
+
+    stem = filepath.stem
+    chunks = stem.split("-")
+
+    data_dict = {}
+
+    for chunk_key, chunk in zip(ASF_CHUNK_ORDER, chunks):
+        data_dict[chunk_key] = chunk
+
+    return data_dict
+
+
+class ASFDataFile:
+    """
+    Represents a single .tiff file and associated annotations.
+
+    Loads and maintains reference to annotation information without needing to load measurement data.
+
+    Loads measurement data into memory when explicitly called.
+    """
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.filestem = self._find_filestem(filename)
+        self.asf_metadata = parse_asf_data_from_filename(filename)
+
+        self.polarization = self.asf_metadata["process_level_class_polarization"]
+        self.beam_mode = self.asf_metadata["beam_mode"]
+        self.geo_bounds = np.zeros((4, 2))
+
+        self.annotation_filepath = None
+        self.annotation_filename = None
+
+        self._load_annotation_file()
+        self._load_annotation_data()
+
+    @staticmethod
+    def _find_filestem(filename: str) -> str:
+        filepath = Path(filename)
+        return filepath.stem
+
+    def _load_annotation_file(self) -> None:
+        datafile_path = Path(self.filename)
+        product_path = datafile_path.parent.parent
+        annotation_path = product_path / "annotation" / f"{self.filestem}.xml"
+        self.annotation_filepath = annotation_path
+        self.annotation_filename = str(annotation_path)
+
+    def _load_annotation_data(self) -> None:
+        ann_tree = ET.parse(self.annotation_filepath)
+        ann_root = ann_tree.getroot()
+        geolocation_grid_element = ann_root.find("geolocationGrid")
+        grid_point_list = geolocation_grid_element.find("geolocationGridPointList")
+        geolocation_array = np.array([
+            (
+                int(p.find("line").text),
+                int(p.find("pixel").text),
+                float(p.find("latitude").text),
+                float(p.find("longitude").text)
+            ) for p in grid_point_list.findall("geolocationGridPoint")
+        ])
+
+        four_corner_indices = [
+            np.argmin(geolocation_array[:, 2]),
+            np.argmax(geolocation_array[:, 2]),
+            np.argmin(geolocation_array[:, 3]),
+            np.argmax(geolocation_array[:, 3]),
+        ]
+
+        four_corner_coords = np.take(geolocation_array, four_corner_indices, axis=0)[:, 2:]
+
+        self.geo_bounds = four_corner_coords
 
 
 class ASFData:
-    def __init__(self):
+    def __init__(self) -> None:
         self.filepath = None
         self.file_stem = None
 
@@ -23,16 +122,10 @@ class ASFData:
         self.filepath = filepath
         filepath_p = Path(filepath)
         self.file_stem = filepath_p.stem
-        self.parse_info_from_stem(self.file_stem)
+        self.parse_info_from_filename(self.file_stem)
 
-    def parse_info_from_stem(self, stem: str) -> None:
-        chunks = stem.split("-")
-        self.mission = chunks[0]
-        self.beam_mode = chunks[1]
-        self.product_type_resolution = chunks[2]
-        self.process_level_class_polarization = chunks[3]
-        self.start_date_time = chunks[4]
-        self.end_date_time = chunks[5]
-        self.absolute_orbit_number = chunks[6]
-        self.mission_data_take_id = chunks[7]
-        self.product_unique_identifier = chunks[8]
+    def parse_info_from_filename(self, filename: Union[str, Path]) -> None:
+        data_dict = parse_asf_data_from_filename(filename)
+
+        for k, v in data_dict.items():
+            self.__setattr__(k, v)
--- tests/asf_analysis/test_asf_data.py
+++ tests/asf_analysis/test_asf_data.py
@@ -1,11 +1,86 @@
+import os
+from pathlib import Path
+
+import numpy as np
 import pytest
 
-from asf_analysis.asf_data import ASFData
+from asf_analysis.asf_data import ASFData, ASFDataFile, parse_asf_data_from_filename, ASF_CHUNK_ORDER
 
 
 @pytest.fixture
 def s1a_filename():
-    yield "/s1a-iw1-slc-vh-20180606t141443-20180606t141508-022237-0267eb-001.tiff"
+    yield (
+        "./data/sentinel-1/S1A_IW_SLC__1SDV_20180606T141441_20180606T141508_022237_0267EB_EC03.SAFE"
+        "/measurement/s1a-iw1-slc-vh-20180606t141443-20180606t141508-022237-0267eb-001.tiff"
+    )
+
+
+@pytest.fixture
+def s1a_asf_datafile(s1a_filename):
+    yield ASFDataFile(s1a_filename)
+
+
+@pytest.mark.unit
+def test_parse_asf_data_from_filename_str(s1a_filename):
+    meta_dict = parse_asf_data_from_filename(s1a_filename)
+
+    attrs = ASF_CHUNK_ORDER
+    for attr in attrs:
+        assert attr in meta_dict
+
+
+@pytest.mark.unit
+def test_parse_asf_data_from_filename_path(s1a_filename):
+    s1a_file_path = Path(s1a_filename)
+    meta_dict = parse_asf_data_from_filename(s1a_file_path)
+
+    attrs = ASF_CHUNK_ORDER
+    for attr in attrs:
+        assert attr in meta_dict
+
+
+class TestASFDataFile:
+    @pytest.mark.unit
+    def test_create_asf_data_file(self, s1a_filename):
+        asf_datafile = ASFDataFile(s1a_filename)
+
+    @pytest.mark.unit
+    def test_asf_data_file_has_attrs(self, s1a_asf_datafile):
+        for attr in ASF_CHUNK_ORDER:
+            assert attr in s1a_asf_datafile.asf_metadata
+
+        assert s1a_asf_datafile.polarization == "vh"
+        assert s1a_asf_datafile.beam_mode == "iw1"
+
+    @pytest.mark.unit
+    def test_asf_data_find_file_stem(self, s1a_filename):
+        true_stem = s1a_filename.split("/")[-1].split(".")[0]
+        assert ASFDataFile._find_filestem(s1a_filename) == true_stem
+
+    @pytest.mark.unit
+    def test_asf_data_file_locates_annotation_file(self, s1a_asf_datafile):
+        assert hasattr(s1a_asf_datafile, "annotation_filename")
+        assert isinstance(s1a_asf_datafile.annotation_filename, str)
+        assert os.path.isfile(s1a_asf_datafile.annotation_filename)
+
+    @pytest.mark.unit
+    def test_has_geo_bounds(self, s1a_asf_datafile):
+        assert hasattr(s1a_asf_datafile, "geo_bounds")
+        geo_bounds = s1a_asf_datafile.geo_bounds
+
+        assert isinstance(geo_bounds, np.ndarray)
+        assert geo_bounds.shape[0] == 4
+        assert geo_bounds.shape[1] == 2
+
+        actual_geo_bounds = np.array([
+            [41.08364918, -120.83964114],
+            [42.74025269, -121.51561604],
+            [41.23010369, -121.88101754],
+            [42.593359434, -120.4492047],
+        ])
+
+        assert np.allclose(geo_bounds, actual_geo_bounds)
+
 
 class TestASFData:
     @pytest.mark.unit
@@ -22,14 +97,14 @@ class TestASFData:
         assert asf_data.filepath == path_to_file
 
     @pytest.mark.unit
-    def test_parse_info_from_stem(self, s1a_filename):
-        stem = s1a_filename.split(".")[0]
+    def test_parse_info_from_filename(self, s1a_filename):
         asf_data = ASFData()
-        asf_data.parse_info_from_stem(stem)
+        asf_data.parse_info_from_filename(s1a_filename)
 
         assert asf_data.absolute_orbit_number == "022237"
 
     @pytest.mark.unit
+    @pytest.mark.skip(reason="may change implementation details...")
     def test_asf_data_has_annotation_xml(self):
         asf_data = ASFData()
         asf_data.load("./data/sentinel-1/")
