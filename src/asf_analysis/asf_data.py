6ad3c404d8917782e7a001d3f3b8aa56a6351964
alakocy <alakocy@gmail.com>
2024-12-28 10:08:26 -0800

    [ENH] Remove ASFData class, change to ASFDataScene and ASFDataFile

--- src/asf_analysis/asf_data.py
+++ src/asf_analysis/asf_data.py
@@ -1,13 +1,19 @@
-import os
-import zipfile
-from pathlib import Path
-from typing import Union
-
-import xml.etree.ElementTree as ET
-
+import glob
+import logging
 import numpy as np
 import rasterio
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+import os
+from pathlib import Path
 from scipy.interpolate import griddata
+from typing import Union
+import xml.etree.ElementTree as ET
+import zipfile
+
+
+module_logger = logging.getLogger(__name__)
+module_logger.setLevel(logging.INFO)
 
 ASF_CHUNK_ORDER = [
     "mission",
@@ -75,6 +81,7 @@ class ASFDataFile:
         return filepath.stem
 
     def _load_annotation_file(self) -> None:
+        """Locate annotation file in ASF tree structure."""
         datafile_path = Path(self.filename)
         product_path = datafile_path.parent.parent
         annotation_path = product_path / "annotation" / f"{self.filestem}.xml"
@@ -120,6 +127,10 @@ class ASFDataFile:
             with rasterio.open(self.filename, "r") as raster_data:
                 self.data_array = raster_data.read(1)
 
+    def _confirm_data_loaded(self) -> None:
+        if self.data_array is None:
+            raise ValueError(f"Data array not loaded yet. Call ASFDataFile.load_data_array() first.")
+
     def get_geo_arrays(self, line_skip: int = 1, pixel_skip: int = 1) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
         Compute geo-referenced data array for the given sampling.
@@ -140,6 +151,8 @@ class ASFDataFile:
             (np.ndarray): longitude of data points
             (np.ndarray): data points
         """
+        self._confirm_data_loaded()
+
         d_shape = self.data_array.shape
         lines = self.geolocation_array[:, 0]
         pixels = self.geolocation_array[:, 1]
@@ -159,6 +172,33 @@ class ASFDataFile:
 
         return lat_grid, lon_grid, reduced_data_array
 
+    def plot_magnitude_data(self, log_transform: bool = False, *args, **kwargs) -> Axes:
+        self._confirm_data_loaded()
+        lat_grid, lon_grid, reduced_data_array = self.get_geo_arrays(50, 50)
+        lat_f, lon_f, data_f = lat_grid.flatten(), lon_grid.flatten(), reduced_data_array.flatten()
+
+        title_prefix = ""
+        if log_transform:
+            data_f = np.log(data_f + 1)
+            title_prefix = "Log "
+
+        fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+        ax.scatter(lon_f, lat_f, c=np.abs(data_f), s=1, *args, **kwargs)
+        ax.set_title(f"{title_prefix}Magnitude Data: {self.polarization.upper()}-{self.beam_mode.upper()}")
+        ax.set_xlabel("Longitude [deg]")
+        ax.set_ylabel("Latitude [deg]")
+        fig.suptitle(f"{self.filestem}")
+        return ax
+
+    # TODO: Add phase plot method
+    # def plot_phase_data(self, *args, **kwargs) -> Axes:
+    #     self._confirm_data_loaded()
+
+
+def make_dir_if_not_exists(extract_location: str) -> None:
+    if not os.path.isdir(extract_location):
+        os.mkdir(extract_location)
+
 
 class ASFDataScene:
     """
@@ -172,69 +212,180 @@ class ASFDataScene:
 
     Contains methods for rendering preview image, finding geographic extents of sub-measurements, etc.
     """
-    def __init__(self, zipfilename: str) -> None:
-        self.zipfilename = zipfilename
-        self.root_dir = None
+
+    # TODO: Write method to check that contents are all present
+    def __init__(self, safe_path: Union[str, Path], zip_path: Union[str, Path] = None, extracted: bool = False) -> None:
+        """
+        Default constructor for ASFDataScene.
+
+        Parameters
+        ----------
+        safe_path: Path or str to .SAFE directory for extracted files
+        zip_path
+        extracted
+        """
+
+        if not isinstance(safe_path, Path):
+            safe_path = Path(safe_path)
+        self.safe_path = safe_path
+        self.extracted = extracted
+
+        if zip_path is not None:
+            if not isinstance(zip_path, Path):
+                zip_path = Path(zip_path)
+            self.zip_path = zip_path
+        else:
+            self.zip_path = None
+
+        self._local_search_for_zipfile(safe_path)
+
+        self.asf_data_files = []
 
         self.data_filenames = []
         self.annotation_filenames = []
-        self._parse_zip_file()
+        self.corner_coordinates = []
+
+        if not self.extracted:
+            self._parse_zip_file()
+        else:
+            self._parse_safe_file()
+
+    @classmethod
+    def from_zipfile(cls, zip_path: Union[str, Path], safe_path: Union[str, Path] = None):
+        """
+        Factory method for creating ASFDataScene directly from zip archive file.
+
+        Assumes zip archive has not been extracted. If it has been, use default constructor: ASFDataScene() with path
+         to SAFE directory as the argument.
+
+        Parameters
+        ----------
+        zip_path
+        safe_path
+
+        Returns
+        -------
+        ASFDataScene instance created from zip archive file.
+
+        """
+        if safe_path is None:
+            safe_path = cls._get_safe_path(zip_path)
+            if safe_path is None:
+                zip_path = Path(zip_path)
+                safe_path = zip_path.parent / f"{zip_path.stem}.SAFE"
+            extracted = False
+
+        extracted = os.path.isdir(safe_path)
+
+        asf_datascene = cls(safe_path, zip_path=zip_path, extracted=extracted)
+
+        if asf_datascene.zip_path is None:
+            asf_datascene.zip_path = Path(zip_path).absolute()
+
+        return asf_datascene
+
+    def _local_search_for_zipfile(self, safe_path: Union[str, Path]) -> None:
+        print(f"_local_search_for_zipfile: {safe_path = }")
+        if not isinstance(safe_path, Path):
+            safe_path = Path(safe_path)
+
+        search_str = str(safe_path.parent / safe_path.stem) + ".zip"
+        print(f"_local_search_for_zipfile: {search_str = }")
+        zips = glob.glob(search_str)
+        print(f"_local_search_for_zipfile: {zips = }")
+        if len(zips) > 0:
+            zip_filename = zips[0]
+            self.zip_path = Path(zip_filename).absolute()
+
+    @staticmethod
+    def _get_safe_path(zip_path: Union[str, Path]) -> Path | None:
+        if not isinstance(zip_path, Path):
+            zip_path = Path(zip_path)
+
+        safe = glob.glob(str(zip_path.parent / zip_path.stem) + ".SAFE")
+        if len(safe) > 0:
+            return Path(safe[0]).absolute()
+        else:
+            return None
 
     def _parse_zip_file(self) -> None:
-        if not os.path.isfile(self.zipfilename):
-            raise ValueError(f"Could not find zip archive file at: {self.zipfilename}")
+        if not os.path.isfile(self.zip_path):
+            raise ValueError(f"Could not find zip archive file at: {self.zip_path}")
         data_stems = []
-        with zipfile.ZipFile(self.zipfilename, "r") as zip_file:
+
+        with zipfile.ZipFile(self.zip_path, "r") as zip_file:
+            # Find measurement files
             for z_name in zip_file.namelist():
                 z_path = Path(z_name)
                 if z_path.suffix == ".tiff":
+                    # asf_data_file = ASFDataFile(str(z_path))
+                    # self.asf_data_files.append(asf_data_file)
                     self.data_filenames.append(str(z_path))
                     data_stems.append(z_path.stem)
+            # Find associated annotation files
             for z_name in zip_file.namelist():
                 z_path = Path(z_name)
                 if z_path.stem in data_stems and z_path.suffix == ".xml":
                     self.annotation_filenames.append(str(z_path))
 
+    def _parse_safe_file(self) -> None:
+        if not os.path.isdir(self.safe_path):
+            raise ValueError(f"Could not find SAFE directory at: {self.safe_path}")
+        data_stems = []
+
+        measure_globs = glob.glob(str(self.safe_path / "measurement" / "*.tiff"))
+        for m_g in measure_globs:
+            asf_data_file = ASFDataFile(str(m_g))
+            self.asf_data_files.append(asf_data_file)
+            self.data_filenames.append(str(Path(m_g).absolute()))
+
+        annotate_globs = glob.glob(str(self.safe_path / "annotation" / "*.xml"))
+        for a_g in annotate_globs:
+            self.annotation_filenames.append(str(Path(a_g).absolute()))
+
+    def extract(self, extract_location: str = None, **kwargs) -> None:
+        """Pulls files out of zip archive into extract_location.
+        Defaults to pull all files, can include `members` kwarg for specific files/directories only.
+        """
+        if extract_location is None:
+            extract_location = self.safe_path.parent
+        else:
+            make_dir_if_not_exists(extract_location)
+
+        with zipfile.ZipFile(self.zip_path, "r") as zip_file:
+            zip_file.extractall(path=extract_location, **kwargs)
+
+        # root_stem = Path(self.zipfilename).stem
+        # self.safe_path = Path(extract_location) / (str(root_stem) + ".SAFE")
+        self.extracted = True
+
+    def locate(self) -> None:
+        map_overlay_file = self.safe_path / "preview" / "map-overlay.kml"
+        if not self.extracted:
+            module_logger.warning(f"Archive not extracted yet. Try calling ASFDataScene.extract() first.")
+        if not os.path.exists(map_overlay_file):
+            raise FileNotFoundError(f"File not found at: {map_overlay_file}")
+        self.corner_coordinates = self._get_corner_coords_from_map_overlay(map_overlay_file)
+
+    @staticmethod
+    def _get_corner_coords_from_map_overlay(kml_file: str) -> list:
+        tree = ET.parse(kml_file)
+        root = tree.getroot()
+        namespaces = {"gx": "http://www.google.com/kml/ext/2.2"}
+        coordinates_tag = root.find(".//gx:LatLonQuad/coordinates", namespaces)
+        coordinates_text = coordinates_tag.text.strip()
+        coordinates = [
+            tuple(map(float, coord.split(","))) for coord in coordinates_text.split()
+        ]
+        return coordinates
+
     # TODO: Add logic for detecting number of sub-swaths and polarization for sub-swaths?
 
-    # TODO: Add method for rendering preview image (preview / quick-look.png) along with North arrow?
-
-    # TODO: Add method for getting 4-point geocode lat/long from preview (very rough)
+    # TODO: Add North arrow? and center coordinates?
+    def plot_preview(self, *args, **kwargs) -> Axes:
+        img = plt.imread(self.safe_path / "preview" / "quick-look.png")
+        fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+        ax.imshow(img, *args, **kwargs)
+        return ax
 
     # TODO: Add method for finding geographic bounds of all sub-measurements and polarizations
-
-
-class ASFData:
-    """
-    Deprecating this in favor of more specific class names.
-
-    Will move most logic into `ASFDataScene` class.
-    """
-    def __init__(self) -> None:
-        self.filepath = None
-        self.file_stem = None
-
-        self.mission = None
-        self.beam_mode = None
-        self.product_type_resolution = None
-        self.process_level_class_polarization = None
-        self.start_date_time = None
-        self.end_date_time = None
-        self.absolute_orbit_number = None
-        self.mission_data_take_id = None
-        self.product_unique_identifier = None
-
-    # TODO: Change to reference scene directory, i.e. S1A...SAFE
-    #  This way, can load any of the tiffs or annotations.
-    #  May need a Scene class?
-    def load(self, filepath: str) -> None:
-        self.filepath = filepath
-        filepath_p = Path(filepath)
-        self.file_stem = filepath_p.stem
-        self.parse_info_from_filename(self.file_stem)
-
-    def parse_info_from_filename(self, filename: Union[str, Path]) -> None:
-        data_dict = parse_asf_data_from_filename(filename)
-
-        for k, v in data_dict.items():
-            self.__setattr__(k, v)
--- tests/asf_analysis/test_asf_data.py
+++ tests/asf_analysis/test_asf_data.py
@@ -1,20 +1,23 @@
 import glob
 import os
+import shutil
+import time
 from pathlib import Path
 
+import matplotlib
 import numpy as np
 import pytest
 from matplotlib import pyplot as plt
 
-from asf_analysis.asf_data import ASFData, ASFDataFile, parse_asf_data_from_filename, ASF_CHUNK_ORDER, ASFDataScene
+from asf_analysis.asf_data import ASFDataFile, parse_asf_data_from_filename, ASF_CHUNK_ORDER, ASFDataScene
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def s1a_all_filenames():
     yield glob.glob("./data/sentinel-1/S1A_IW_SLC__1SDV_20180606T141441_*.SAFE/measurement/*.tiff")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def single_s1a_filename():
     yield (
         "./data/sentinel-1/S1A_IW_SLC__1SDV_20180606T141441_20180606T141508_022237_0267EB_EC03.SAFE"
@@ -22,9 +25,14 @@ def single_s1a_filename():
     )
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def s1a_zipfile():
-    yield "./data/sentinel-1/S1A_IW_SLC__1SDV_20180606T141441_20180606T141508_022237_0267EB_EC03.zip"
+    yield "./tests/test_data/zip/S1A_IW_SLC__1SDV_20180606T141441_20180606T141508_022237_0267EB_EC03.zip"
+
+
+@pytest.fixture(scope="session")
+def s1a_safedir():
+    yield "./data/sentinel-1/S1A_IW_SLC__1SDV_20180606T141441_20180606T141508_022237_0267EB_EC03.SAFE"
 
 
 @pytest.fixture
@@ -49,6 +57,15 @@ def test_parse_asf_data_from_filename_path(single_s1a_filename):
     attrs = ASF_CHUNK_ORDER
     for attr in attrs:
         assert attr in meta_dict
+
+
+@pytest.mark.unit
+def test_parse_info_from_filename(single_s1a_filename):
+    info_dict = parse_asf_data_from_filename(single_s1a_filename)
+    assert info_dict["mission"] == "s1a"
+    assert info_dict["process_level_class_polarization"] == "vh"
+    assert info_dict["absolute_orbit_number"] == "022237"
+    assert info_dict["mission_data_take_id"] == "0267eb"
 
 
 class TestASFDataFile:
@@ -167,70 +184,179 @@ class TestASFDataFile:
         fig.suptitle(asf_datafile.filestem)
         plt.show()
 
+    @pytest.mark.plot
+    def test_plot_magnitude(self, single_s1a_filename):
+        asf_datafile = ASFDataFile(single_s1a_filename)
+        asf_datafile.load_data_array()
+        ax = asf_datafile.plot_magnitude_data(log_transform=True, cmap="gray")
+        plt.show()
+
+    @pytest.mark.unit
+    def test_confirm_data_loaded(self, single_s1a_filename):
+        asf_datafile = ASFDataFile(single_s1a_filename)
+        with pytest.raises(ValueError):
+            asf_datafile._confirm_data_loaded()
+
+        asf_datafile.load_data_array()
+        asf_datafile._confirm_data_loaded()
+
+@pytest.fixture(scope="function")
+def asf_datascene(s1a_zipfile):
+    asf_datascene = ASFDataScene.from_zipfile(s1a_zipfile)
+    yield asf_datascene
+
+
+@pytest.fixture(scope="session")
+def asf_extracted_safe(s1a_zipfile):
+    safe_stem = Path(s1a_zipfile).stem
+    safe_dir = Path(s1a_zipfile).parent.parent / "safe"
+    desired_safe_destination = safe_dir / (safe_stem + ".SAFE")
+    asf_data_scene = ASFDataScene.from_zipfile(s1a_zipfile, safe_path=desired_safe_destination)
+    # asf_data_scene.safe_path = Path(f"./tests/test_data/safe/{asf_data_scene.safe_path.name}")
+    if not os.path.isdir(asf_data_scene.safe_path):
+        print("EXTRACTING SAFE DATA FROM ZIP ARCHIVE...")
+        asf_data_scene.extract()
+    yield asf_data_scene.safe_path
+    # As part of teardown, delete SAFE path contents. Will make tests run much longer...
+    inuse = True
+    while inuse:
+        try:
+            shutil.rmtree(asf_data_scene.safe_path)
+            inuse = False
+        except PermissionError as pe:
+            print(f"File still in use:\n{pe}")
+            time.sleep(2.0)  # Wait for system to realize it's done with this directory...
 
 class TestASFDataScene:
     @pytest.mark.unit
-    def test_create_asf_datascene(self, s1a_zipfile):
-        asf_datascene = ASFDataScene(s1a_zipfile)
-        assert asf_datascene.zipfilename == s1a_zipfile
-        assert asf_datascene.root_dir is None
+    def test_create_asf_datascene_from_zipfile_only(self, s1a_zipfile):
+        """
+        1. Zip archive file only, no SAFE directory. Should instantiate safe_path in same parent folder.
+        """
+        asf_data_scene = ASFDataScene.from_zipfile(s1a_zipfile)
+        assert asf_data_scene.zip_path.resolve() == Path(s1a_zipfile).resolve()
+        assert not os.path.exists(asf_data_scene.safe_path)
 
     @pytest.mark.unit
-    def test_asf_datascene_finds_data_files(self, s1a_zipfile):
+    def test_create_asf_datascene_from_zipfile_explicit_safe_dir(self, s1a_zipfile, tmp_path):
+        """
+        2. Zip archive file only, with explicit path to safe dir (does not exist yet).
+        """
+        safe_stem = Path(s1a_zipfile).stem
+        desired_safe_destination = tmp_path / (safe_stem + ".SAFE")
+        asf_data_scene = ASFDataScene.from_zipfile(s1a_zipfile, safe_path=desired_safe_destination)
+        assert asf_data_scene.safe_path.resolve() == desired_safe_destination.resolve()
+        assert not os.path.exists(asf_data_scene.safe_path)
+
+    @pytest.mark.unit
+    def test_create_asf_datascene_from_SAFE(self, asf_extracted_safe):
+        """
+        3. SAFE directory only (no local zip). Zip path is None, parsing uses glob
+        """
+        asf_data_scene = ASFDataScene(asf_extracted_safe, extracted=True)
+        assert isinstance(asf_data_scene, ASFDataScene)
+        assert asf_data_scene.safe_path == asf_extracted_safe
+        # assert Path(asf_datascene.zipfilename).absolute().parent == Path(s1a_zipfile).absolute().parent
+        assert asf_data_scene.zip_path is None
+
+    @pytest.mark.unit
+    def test_create_asf_datascene_from_SAFE_with_zip(self, s1a_safedir):
+        """
+        4. SAFE directory and local zip. Expect that constructor finds zip archive and parses based on glob.
+        """
+        asf_data_scene = ASFDataScene(s1a_safedir, extracted=True)
+        assert isinstance(asf_data_scene, ASFDataScene)
+        assert asf_data_scene.safe_path.resolve() == Path(s1a_safedir).resolve()
+
+        file_stem = Path(s1a_safedir).stem
+        expected_zip_path = Path(s1a_safedir).parent / f"{file_stem}.zip"
+        assert asf_data_scene.zip_path.resolve() == expected_zip_path.resolve()
+
+    @pytest.mark.unit
+    def test_asf_datascene_finds_data_files(self, asf_datascene):
         """On load, want ASFDataScene to index data files within ZIP archive"""
-        asf_datascene = ASFDataScene(s1a_zipfile)
         assert hasattr(asf_datascene, "data_filenames")
         assert isinstance(asf_datascene.data_filenames, list)
         assert len(asf_datascene.data_filenames) == 6
 
     @pytest.mark.unit
-    def test_asf_datascene_finds_annotation_files(self, s1a_zipfile):
+    def test_asf_datascene_finds_annotation_files(self, asf_datascene):
         """On load, want ASFDataScene to index annotation files within ZIP archive"""
-        asf_datascene = ASFDataScene(s1a_zipfile)
         assert hasattr(asf_datascene, "annotation_filenames")
         assert isinstance(asf_datascene.annotation_filenames, list)
         assert len(asf_datascene.annotation_filenames) == 6
 
+    @pytest.mark.unit
+    def test_asf_datascene_extract_selected_files(self, s1a_zipfile, tmp_path):
+        out_dir = str(tmp_path / "S1A_test")
+        asf_datascene = ASFDataScene.from_zipfile(s1a_zipfile)
+        assert not (os.path.isdir(out_dir))
+        # base = "S1A_IW_SLC__1SDV_20180606T141441_20180606T141508_022237_0267EB_EC03.SAFE/"
+        subdirs = ["annotation/", "preview/", "support/"]
+        zip_path = Path(s1a_zipfile)
+        base = zip_path.stem + ".SAFE/"
 
-    # # On load, want the ASFDataScene to find the subfiles and store a dict of {stem:geo_bounds}
+        asf_datascene.extract(
+            extract_location=out_dir,
+            members=[base + subdir for subdir in subdirs]
+        )
+        assert os.path.isdir(out_dir)
+
+        assert asf_datascene.safe_path is not None
+
+        for member_dir in subdirs:
+            assert os.path.isdir(f"{out_dir}\\{base}\\{member_dir}")
+
+    @pytest.mark.unit
+    def test_asf_datascene_has_four_geo_corners(self, s1a_safedir, s1a_zipfile, tmp_path):
+        asf_datascene = ASFDataScene(s1a_safedir)
+        assert asf_datascene.zip_path.resolve() == (Path(s1a_safedir).parent / f"{Path(s1a_safedir).stem}.zip").resolve()
+        assert hasattr(asf_datascene, "corner_coordinates")
+        assert isinstance(asf_datascene.corner_coordinates, list)
+
+        zip_path = Path(s1a_zipfile)
+        base = zip_path.stem + ".SAFE/"
+
+        asf_datascene.extract(
+            extract_location=tmp_path,
+            members=[base + "preview/", base + "preview/map-overlay.kml"]
+        )
+        assert asf_datascene.corner_coordinates == []
+
+        asf_datascene.locate()
+        assert asf_datascene.corner_coordinates != []
+
+    @pytest.mark.unit
+    def test_asf_datascene_show_preview(self, s1a_safedir):
+        asf_data_scene = ASFDataScene(safe_path=s1a_safedir, extracted=True)
+        ax = asf_data_scene.plot_preview()
+        assert isinstance(ax, matplotlib.axes.Axes)
+
+    @pytest.mark.unit
+    def test_asf_datascene_from_zip_creates_sub_data_files(self, s1a_zipfile):
+        """Currently, we are not loading measurement and annotation files from zip. Need to be extracted"""
+        asf_data_scene = ASFDataScene(s1a_zipfile)
+
+        assert hasattr(asf_data_scene, "asf_data_files")
+        assert len(asf_data_scene.asf_data_files) == 0
+
+    @pytest.mark.unit
+    def test_asf_datascene_creates_sub_data_files(self, s1a_safedir):
+        asf_data_scene = ASFDataScene(s1a_safedir, extracted=True)
+
+        assert hasattr(asf_data_scene, "asf_data_files")
+        assert isinstance(asf_data_scene.asf_data_files, list)
+        for data_file in asf_data_scene.asf_data_files:
+            assert isinstance(data_file, ASFDataFile)
+            assert isinstance(data_file.annotation_filepath, Path)
+            assert isinstance(data_file.annotation_filename, str)
+
+    # TODO: On load, want the ASFDataScene to find the subfiles and store a dict of {stem:geo_bounds}
     # @pytest.mark.unit
     # def test_asf_datascene_finds_geo_bounds(self, s1a_zipfile):
     #     asf_datascene = ASFDataScene(s1a_zipfile)
     #     assert hasattr(asf_datascene, "stem_geo_bounds")
 
-
-# TODO: Update this to hold entire .SAFE directory data
-#  Add methods for "mosaicing" multiple georeferenced coords into a plottable structure?
-#  Pre-prepared plotting methods (Magnitude top, phase bottom; columns for VH versus VV)
-#  False color composite ability
-class TestASFData:
-    @pytest.mark.unit
-    def test_create_asf_data(self, tmpdir):
-        asf_data = ASFData()
-        assert asf_data.filepath is None
-
-    @pytest.mark.unit
-    def test_load_asf_file(self, tmpdir, single_s1a_filename):
-        path_to_file = tmpdir + single_s1a_filename
-        asf_data = ASFData()
-        asf_data.load(path_to_file)
-
-        assert asf_data.filepath == path_to_file
-
-    @pytest.mark.unit
-    def test_parse_info_from_filename(self, single_s1a_filename):
-        asf_data = ASFData()
-        asf_data.parse_info_from_filename(single_s1a_filename)
-
-        assert asf_data.absolute_orbit_number == "022237"
-
-    @pytest.mark.unit
-    @pytest.mark.skip(reason="may change implementation details...")
-    def test_asf_data_has_annotation_xml(self):
-        asf_data = ASFData()
-        asf_data.load("./data/sentinel-1/")
-
-
-# my_data = ASFData()
-#
-# my_data.load(filepath)
+    # TODO: Add methods for "mosaicing" multiple georeferenced coords into a plottable structure?
+    #  Pre-prepared plotting methods (Magnitude top, phase bottom; columns for VH versus VV)
+    #  False color composite ability
